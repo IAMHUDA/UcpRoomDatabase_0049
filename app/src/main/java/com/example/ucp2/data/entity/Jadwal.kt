3879aa9413c2aae35523ed7128d737c21797303f
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-18 17:43:02 +0700

    add jadwaldao

--- app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "jadwal")
 data class Jadwal(
     @PrimaryKey
-    val id: String,
+    val idJadwal: String,
     val NamaDokter: String,
     val NamaPasien: String,
     val noHp: String,
