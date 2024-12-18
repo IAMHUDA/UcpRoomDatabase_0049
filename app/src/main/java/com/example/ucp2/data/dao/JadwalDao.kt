694b4a9ff427f854d109854139105ba7d4084197
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-18 17:36:12 +0700

    add jadwaldao

--- app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
@@ -1,9 +1,32 @@
 package com.example.ucp2.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface JadwalDao {
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
 
+    //fungsi get all data
+    @Query("SELECT * FROM jadwal ORDER BY NamaDokter ASC")
+    fun getAlljadwal() : Flow<List<Jadwal>>
+
+    //get jadwal
+    @Query("SELECT * FROM jadwal WHERE  NamaDokter= :NamaDokter")
+    fun getjadwal(NamaDokter: String): Flow<Jadwal>
+
+    //delete jadwal
+    @Delete
+    suspend fun  deletejadwal(jadwal: Jadwal)
+
+    //update jadwal
+    @Update
+    suspend fun  updatejadwal(jadwal: Jadwal)
 
 }
