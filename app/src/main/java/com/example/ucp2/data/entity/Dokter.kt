bacad0e7c67fc26816e36ee8912af64d4dc35456
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-18 17:22:25 +0700

    add data class dokter

--- app/src/main/java/com/example/ucp2/data/entity/Dokter.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Dokter.kt
@@ -1,3 +1,15 @@
 package com.example.ucp2.data.entity
 
-data class Dokter()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dokter")
+data class Dokter(
+    @PrimaryKey
+    val id: String,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val noHp: String,
+    val jamKerja: String
+)
