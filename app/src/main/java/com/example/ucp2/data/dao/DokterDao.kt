dd9baac3f3c31c2cf6d54caed5a7976cc04ecaaf
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-18 17:30:48 +0700

    add dao dokter

--- app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
@@ -1,21 +1,19 @@
 package com.example.ucp2.data.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import com.example.ucp2.data.entity.Dokter
 import kotlinx.coroutines.flow.Flow
 
 
+@Dao
 interface DokterDao {
 
     @Insert
     suspend fun insertDokter(dokter:Dokter)
 
-    //fungsi get all data
-    @Query("SELECT * FROM dokter ORDER BY nama ASC")
-    fun getALLDokter() : Flow<List<Dokter>>
-
     //get dokter
-    @Query("SELECT * FROM dokter")
+    @Query("SELECT * FROM dokter WHERE id = :id")
+    fun getDokter(nim:String):Flow<Dokter>
 }
--- app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/JadwalDao.kt
@@ -1,4 +1,9 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Dao
+
+@Dao
 interface JadwalDao {
+
+
 }
