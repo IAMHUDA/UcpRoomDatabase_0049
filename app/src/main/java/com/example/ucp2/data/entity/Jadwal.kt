6dfdb8302642c08c2ef334393748e13b38b267e8
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-18 17:25:06 +0700

    add data class jadwal

--- app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
@@ -1,3 +1,15 @@
 package com.example.ucp2.data.entity
 
-data class Jadwal()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "jadwal")
+data class Jadwal(
+    @PrimaryKey
+    val id: String,
+    val NamaDokter: String,
+    val NamaPasien: String,
+    val noHp: String,
+    val TanggalKonsul: String,
+    val Status: String
+)
