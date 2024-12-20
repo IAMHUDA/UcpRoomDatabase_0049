678a83b61c4df2a8d6b1e4190f6662a6cb8f45c6
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-20 07:55:55 +0700

    edit rsdatabase

--- app/src/main/java/com/example/ucp2/data/database/RsDatabase.kt
+++ app/src/main/java/com/example/ucp2/data/database/RsDatabase.kt
@@ -1,3 +1,35 @@
 package com.example.ucp2.data.database
 
-data class RsDatabase()
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.DokterDao
+import com.example.ucp2.data.dao.JadwalDao
+import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.data.entity.Jadwal
+
+@Database(entities = [Dokter::class, Jadwal::class], version = 1, exportSchema = false)
+abstract class RsDatabase : RoomDatabase() {
+
+    // Mendefinisikan fungsi untuk mengakses DAO Dokter dan Jadwal
+    abstract fun dokterDao(): DokterDao
+    abstract fun jadwalDao(): JadwalDao
+
+    companion object {
+        @Volatile
+        private var instance: RsDatabase? = null
+
+        fun getDatabase(context: Context): RsDatabase {
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    RsDatabase::class.java,
+                    "RsDatabase"
+                )
+                    .build()
+                    .also { instance = it }
+            }
+        }
+    }
+}
