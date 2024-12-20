94ebcf84e7e5d92d1bf44c3d91e30d06c10a531e
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-21 00:15:22 +0700

    Add repository dokter

--- app/src/main/java/com/example/ucp2/repository/RepositoryDokter.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDokter.kt
@@ -1,6 +1,10 @@
 package com.example.ucp2.repository
 
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryDokter {
     suspend fun addDokter(dokter: Dokter)
-    suspend fun getAllDokter(): List<Dokter>
+    fun getAllDokter(): Flow<List<Dokter>>
+    fun getDokter(idDokter:String): Flow<Dokter>
 }
