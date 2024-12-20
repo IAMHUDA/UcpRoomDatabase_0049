ca8877f3fd7888b249dbb8923cd0188306f4390d
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-20 07:52:42 +0700

    add repo jadwal

--- app/src/main/java/com/example/ucp2/repository/RepositoryJadwal.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryJadwal.kt
@@ -1,4 +1,11 @@
 package com.example.ucp2.repository
 
+import com.example.ucp2.data.entity.Jadwal
+
 interface RepositoryJadwal {
+    suspend fun addJadwal(jadwal: Jadwal)
+    suspend fun getAllJadwal(): List<Jadwal>
+    suspend fun updateJadwal(jadwal: Jadwal)
+    suspend fun deleteJadwal(jadwal: Jadwal)
+    suspend fun getJadwalDetail(id: Int): Jadwal
 }
