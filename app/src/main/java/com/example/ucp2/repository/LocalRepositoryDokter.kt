10e34e7a8138eab3f2bf166a24be15b02c0d2b52
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-20 07:52:57 +0700

    add local repo dokter

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryDokter.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDokter.kt
@@ -1,4 +1,24 @@
 package com.example.ucp2.repository
 
-class LocalRepositoryDokter {
+
+import com.example.ucp2.data.dao.DokterDao
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class LocalRepositoryDokter(private val dokterDao: DokterDao) : RepositoryDokter {
+
+    // Mengganti implementasi addDokter dari interface
+    override suspend fun addDokter(dokter: Dokter) {
+        withContext(Dispatchers.IO) {
+            dokterDao.insertDokter(dokter)
+        }
+    }
+
+    // Mengganti implementasi getAllDokter dari interface
+    override suspend fun getAllDokter(): List<Dokter> {
+        return withContext(Dispatchers.IO) {
+            dokterDao.getAllDokter()
+        }
+    }
 }
