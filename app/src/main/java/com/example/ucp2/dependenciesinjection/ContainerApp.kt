7f157b95d2aa4e90be08073c8b00231555410d78
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-20 07:54:03 +0700

    add containerapp class

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,2 +1,26 @@
 package com.example.ucp2.dependenciesinjection
 
+
+
+import com.example.ucp2.repository.RepositoryJadwal
+import com.example.ucp2.repository.RepositoryDokter
+import android.content.Context
+import com.example.ucp2.data.database.RsDatabase
+import com.example.ucp2.repository.LocalRepositoryDokter
+import com.example.ucp2.repository.LocalRepositoryJadwal
+
+
+interface InterfaceContainerApp {
+    val dokterRepository: RepositoryDokter
+    val jadwalRepository: RepositoryJadwal
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val dokterRepository: RepositoryDokter by lazy {
+        LocalRepositoryDokter(RsDatabase.getDatabase(context).dokterDao())
+    }
+
+    override val jadwalRepository: RepositoryJadwal by lazy {
+        LocalRepositoryJadwal(RsDatabase.getDatabase(context).jadwalDao())
+    }
+}
