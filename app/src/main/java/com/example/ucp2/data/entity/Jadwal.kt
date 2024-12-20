af84771356b2f7527b10f83586ce07a4370ee9ad
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-20 07:54:29 +0700

    changes jadwal dao

--- app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2/data/entity/Jadwal.kt
@@ -1,15 +1,24 @@
 package com.example.ucp2.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "jadwal")
-data class Jadwal(
-    @PrimaryKey
-    val idJadwal: String,
-    val NamaDokter: String,
-    val NamaPasien: String,
-    val noHp: String,
-    val TanggalKonsul: String,
-    val Status: String
+@Entity(
+    tableName = "jadwal",
+    foreignKeys = [ForeignKey(
+        entity = Dokter::class,
+        parentColumns = ["idDokter"],
+        childColumns = ["idDokter"],
+        onDelete = ForeignKey.CASCADE
+    )]
 )
+data class Jadwal(
+    @PrimaryKey(autoGenerate = true) val idJadwal: Int = 0,
+    val idDokter: String,
+    val hari: String,
+    val jamMulai: String,
+    val jamSelesai: String
+)
+
+
