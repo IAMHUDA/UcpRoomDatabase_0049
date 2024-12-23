ca3739f1c0ea20aa0e29dc241a6f0633ecfeb740
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-24 02:55:20 +0700

    Add Alamat navigasi

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -12,7 +12,27 @@ object DestinasiHome : AlamatNavigasi { // Object untuk halaman home
     override val route: String = "home"
 }
 
-object DestinasiDetail : AlamatNavigasi { // Object untuk halaman detail dokter
+object DestinasiInsertJadwal : AlamatNavigasi {
+    override val route: String = "insertjadwal"
+}
+
+object  DestinasiHomeJW : AlamatNavigasi {
+    override val route: String = "home_jw"
+}
+
+object DestinasiDetailJW : AlamatNavigasi {
+    override val route = "detail_jw"
+    const val idJadwal = "idJadwal"
+    val routesWithArg = "$route/{$idJadwal}"
+}
+
+object DestinasiUpdateJadwal : AlamatNavigasi {
+    override val route = " update_jw"
+    const val  idJadwal = " idJadwal"
+    val routesWithArg = "$route/{$idJadwal}"
+}
+
+object DestinasiDetail : AlamatNavigasi {
     override val route: String = "detail"
     const val ID_DOKTER = "idDokter"
     val routesWithArg = "$route/{$ID_DOKTER}"
