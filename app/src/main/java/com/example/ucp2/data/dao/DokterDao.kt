b47db0eddbe4a6571cca957ee0e0c21fb4cf5eb2
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-24 03:06:13 +0700

    add dokter dao

--- app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
@@ -2,6 +2,7 @@ package com.example.ucp2.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.ucp2.data.entity.Dokter
 import kotlinx.coroutines.flow.Flow
@@ -10,10 +11,14 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface DokterDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDokter(dokter:Dokter)
 
-    //get dokter
-    @Query("SELECT * FROM dokter")
-    suspend fun getAllDokter(): List<Dokter>
+    //fungsi get all data
+    @Query("SELECT * FROM dokter ORDER BY nama ASC")
+    fun getAllDokter() : Flow<List<Dokter>>
+
+    //get Mahasiswa
+    @Query("SELECT * FROM dokter WHERE idDokter = :idDokter")
+    fun getDokter(idDokter: String): Flow<Dokter>
 }
