65a031a35012645ed3a96fc2b5527b9ef3179bbd
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-24 02:58:36 +0700

    Add halaman tambah dokter

--- app/src/main/java/com/example/ucp2/ui/view/dokter/InsertDokter.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dokter/InsertDokter.kt
@@ -1,14 +1,20 @@
 package com.example.ucp2.ui.view.dokter
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -39,28 +45,27 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun InsertDokter(
-    onBack:() -> Unit,
+    onBack: () -> Unit,
     onNavigate: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DokterViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){
+) {
     val uiState = viewModel.uiState
-    val snackbarHostState = remember{SnackbarHostState()}
+    val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
-    //Observasi perubahan snackbarMessage
+
     LaunchedEffect(uiState.snackBarMessage) {
         uiState.snackBarMessage?.let { message ->
             coroutineScope.launch {
-                snackbarHostState.showSnackbar(message) //Tampilkan snackbar
+                snackbarHostState.showSnackbar(message)
                 viewModel.resetSnackBarMessage()
             }
         }
     }
 
-    Scaffold (
-        modifier = modifier,
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState)} //Tempatkan snackbar di scaffold
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) { padding ->
         Column(
             modifier = Modifier
@@ -71,17 +76,17 @@ fun InsertDokter(
             TopAppBar(
                 onBack = onBack,
                 showBackButton = true,
-                judul = "Tambah dokter",
+                judul = "Tambah Dokter",
             )
-            //isi body
+            // Isi body
             InsertBodyDokter(
                 uiState = uiState,
-                onValueChange = {updatedEvent ->
-                    viewModel.updateState(updatedEvent) //update state di ViewModel
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)
                 },
                 onClick = {
                     viewModel.saveData()
-                    onNavigate()
+
                 }
             )
         }
@@ -93,25 +98,39 @@ fun InsertBodyDokter(
     modifier: Modifier = Modifier,
     onValueChange: (DokterEvent) -> Unit,
     uiState: DokterUIState,
-    onClick:() -> Unit
-){
-    Column (
+    onClick: () -> Unit
+) {
+    Column(
         modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
-        FormDokter(
-            dokterEvent = uiState.dokterEvent,
-            onValueChange = onValueChange,
-            errorState = uiState.isEntryValid,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button(
-            onClick = onClick,
-            modifier = Modifier.fillMaxWidth()
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth().offset(y = -30.dp),
         ) {
-            Text(text = "Simpan")
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                FormDokter(
+                    dokterEvent = uiState.dokterEvent,
+                    onValueChange = onValueChange,
+                    errorState = uiState.isEntryValid,
+                    modifier = Modifier.weight(1f) // Menggunakan weight agar LazyColumn mengisi ruang
+                )
+
+                Button(
+                    onClick = onClick,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    Text(text = "Simpan", color = Color.White)
+                }
+            }
         }
     }
 }
@@ -124,7 +143,6 @@ fun FormDokter(
     errorState: FormErrorState = FormErrorState(),
     modifier: Modifier = Modifier
 ) {
-
     val specializations = listOf(
         "Umum" to Color(0xFFE57373),      // Red
         "Anak" to Color(0xFF64B5F6),       // Blue
@@ -135,89 +153,123 @@ fun FormDokter(
 
     var selectedSpecialization by remember { mutableStateOf(dokterEvent.spesialis) }
 
-    Column(
-        modifier = modifier.padding(16.dp),
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         // ID Dokter
-        TextField(
-            value = dokterEvent.idDokter,
-            onValueChange = { onValueChange(dokterEvent.copy(idDokter = it)) },
-            label = { Text("ID Dokter") },
-            isError = errorState.idDokter != null
-        )
+        item {
+            TextFieldWithError(
+                value = dokterEvent.idDokter,
+                onValueChange = { onValueChange(dokterEvent.copy(idDokter = it)) },
+                label = "ID Dokter",
+                error = errorState.idDokter
+            )
+        }
 
         // Nama Dokter
-        TextField(
-            value = dokterEvent.nama,
-            onValueChange = { onValueChange(dokterEvent.copy(nama = it)) },
-            label = { Text("Nama Dokter") },
-            isError = errorState.nama != null
-        )
+        item {
+            TextFieldWithError(
+                value = dokterEvent.nama,
+                onValueChange = { onValueChange(dokterEvent.copy(nama = it)) },
+                label = "Nama Dokter",
+                error = errorState.nama
+            )
+        }
 
         // Spesialis
-        Text(
-            text = "Spesialis",
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
-        FlowRow(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalArrangement = Arrangement.Center
-        ) {
-            specializations.forEach { (spesialis, color) ->
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    RadioButton(
-                        selected = selectedSpecialization == spesialis,
-                        onClick = {
-                            selectedSpecialization = spesialis
-                            onValueChange(dokterEvent.copy(spesialis = spesialis))
-                        },
-                        colors = androidx.compose.material3.RadioButtonDefaults.colors(
-                            selectedColor = color,
-                            unselectedColor = Color.Gray
+        item {
+            Text(
+                text = "Spesialis",
+                style = MaterialTheme.typography.titleMedium,
+            )
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(1.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                specializations.forEach { (spesialis, color) ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = selectedSpecialization == spesialis,
+                            onClick = {
+                                selectedSpecialization = spesialis
+                                onValueChange(dokterEvent.copy(spesialis = spesialis))
+                            },
+                            colors = androidx.compose.material3.RadioButtonDefaults.colors(
+                                selectedColor = color,
+                                unselectedColor = Color.Gray
+                            )
                         )
-                    )
-                    Text(
-                        text = spesialis,
-                        modifier = Modifier.padding(start = 8.dp),
-                        style = MaterialTheme.typography.bodyLarge,
-                        color = if (selectedSpecialization == spesialis) color else Color.Gray
-                    )
+                        Text(
+                            text = spesialis,
+                            modifier = Modifier.padding(start = 8.dp),
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = if (selectedSpecialization == spesialis) color else Color.Gray
+                        )
+                    }
                 }
             }
         }
 
         // Klinik
-        TextField(
-            value = dokterEvent.klinik,
-            onValueChange = { onValueChange(dokterEvent.copy(klinik = it)) },
-            label = { Text("Klinik") },
-            isError = errorState.klinik != null
-        )
+        item {
+            TextFieldWithError(
+                value = dokterEvent.klinik,
+                onValueChange = { onValueChange(dokterEvent.copy(klinik = it)) },
+                label = "Klinik",
+                error = errorState.klinik
+            )
+        }
 
         // No HP
-        TextField(
-            value = dokterEvent.noHp,
-            onValueChange = { onValueChange(dokterEvent.copy(noHp = it)) },
-            label = { Text("No. HP") },
-            isError = errorState.noHp != null
-        )
+        item {
+            TextFieldWithError(
+                value = dokterEvent.noHp,
+                onValueChange = { onValueChange(dokterEvent.copy(noHp = it)) },
+                label = "No. HP",
+                error = errorState.noHp
+            )
+        }
 
         // Jam Kerja
-        TextField(
-            value = dokterEvent.jamKerja,
-            onValueChange = { onValueChange(dokterEvent.copy(jamKerja = it)) },
-            label = { Text("Jam Kerja") },
-            isError = errorState.jamKerja != null
-        )
+        item {
+            TextFieldWithError(
+                value = dokterEvent.jamKerja,
+                onValueChange = { onValueChange(dokterEvent.copy(jamKerja = it)) },
+                label = "Jam Kerja",
+                error = errorState.jamKerja
+            )
+        }
     }
 }
 
 
+@Composable
+fun TextFieldWithError(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    error: String?
+) {
+    Column {
+        if (error != null) {
+            Text(
+                text = error,
+                color = Color.Red,
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+            label = { Text(label) },
+            isError = error != null,
+        )
+    }
+}
 
 
 
